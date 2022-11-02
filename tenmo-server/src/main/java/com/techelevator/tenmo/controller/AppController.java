cd0fee432ba1ea1f78d91fec7a5d63167bdfed85
Shao En Hsu <kevinhsu16739@gmail.com>
2022-11-02 16:27:10 -0400

    Finished up to step 4

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AppController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AppController.java
@@ -1,8 +1,24 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.dao.AccountDao;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.security.Principal;
 
 @RestController
 public class AppController {
 
+    private AccountDao accountDao;
+
+    public AppController(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @GetMapping(path = "/balance")
+    public BigDecimal getBalance(Principal principal) {
+        String username = principal.getName();
+        return accountDao.getBalance(username);
+    }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,4 +1,28 @@
 package com.techelevator.tenmo.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
 public class JdbcAccountDao implements AccountDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public BigDecimal getBalance(String username) {
+        String getBalanceSql = "SELECT balance" +
+                " FROM account AS a" +
+                " JOIN tenmo_user AS tu ON a.user_id = tu.user_id" +
+                " WHERE username = ?";
+        BigDecimal currentBalance;
+
+        currentBalance = jdbcTemplate.queryForObject(getBalanceSql, BigDecimal.class, username);
+        return currentBalance;
+    }
 }
