b2fb0bb96e1de1e101aacced2c9898f4f581a51e
Shao En Hsu <kevinhsu16739@gmail.com>
2022-11-03 09:20:20 -0400

    wired up find list of users

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AppController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AppController.java
@@ -1,24 +1,57 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.techelevator.tenmo.dao.UserDao;
+import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.model.User;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
+@PreAuthorize("isAuthenticated()")
 public class AppController {
 
     private AccountDao accountDao;
+    private UserDao userDao;
 
-    public AppController(AccountDao accountDao) {
+    public AppController(AccountDao accountDao, UserDao userDao) {
         this.accountDao = accountDao;
+        this.userDao = userDao;
     }
 
     @GetMapping(path = "/balance")
     public BigDecimal getBalance(Principal principal) {
         String username = principal.getName();
         return accountDao.getBalance(username);
+    }
+
+    //TODO need to only show user_id and username
+    @GetMapping(path = "/users")
+    public List<User> getAllUsers() {
+        return userDao.findAll();
+    }
+
+    //TODO
+    @PostMapping(path = "/transfers")
+    public Transfer sendMoney(@RequestBody Transfer transfer) {
+        return null;
+    }
+
+    //TODO
+    @GetMapping(path = "/transfers/{userId}")
+    public List<Transfer> getAllTransfersByUserId(@PathVariable Long userId) {
+        return null;
+    }
+
+    //TODO
+    @GetMapping(path = "/transfers/{transfersId}")
+    public Transfer getTransferDetailsByTransferId(@PathVariable Long transferId) {
+        return null;
     }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -1,4 +1,9 @@
 package com.techelevator.tenmo.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcTransferDao implements TransferDao {
+
+
 }
